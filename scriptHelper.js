802e9f3800bef7bf25176419501d036b0072c50a
Vi Fendrick <violetynaf@gmail.com>
2023-07-11 19:15:12 -0500

    maybe done ish?

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,38 +2,82 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
+    let div = document.getElementById("missionTarget");
+    div.innerHTML = 
+                `<h2>Mission Destination</h2>
                 <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
+                    <li>Name: ${name}</li>
+                    <li>Diameter: ${diameter}</li>
                     <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
+                    <li>Distance from Earth: ${distance}</li>
+                    <li>Number of Moons: ${moons}</li>
                 </ol>
-                <img src="">
-   */
+                <img src="${imageUrl}">`;      
 }
 
 function validateInput(testInput) {
-   
+   let checkForNumber = Number(testInput);
+   if (testInput === "") {
+    return "Empty";
+   } else if (isNaN(checkForNumber)) {
+    return "Not a Number";
+   } else  if (isNaN(checkForNumber) === false) {
+    return "Is a Number";
+   }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+   let pilot = document.getElementById("pilotName");
+   let copilot = document.getElementById("copilotName");
+   let fuelLevel = document.getElementById("fuelLevel");
+   let cargoLevel = document.getElementById("cargoMass");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
+
+   if (validateInput(pilot) === "Empty" | validateInput(copilot) === "Empty" | validateInput(fuelLevel) === "Empty" | validateInput(cargoLevel) === "Empty") {
+    alert("Please fill in all fields!");
+   } else if (validateInput(pilot) === "Is a Number" | validateInput(copilot) === "Is a Number" | validateInput(fuelLevel) === "Not a Number" | validateInput(cargoLevel) === "Not a Number" ) {
+    alert("Please fill in fields with correct information!")
+   } else {
+    pilotStatus.innerHTML = `Pilot ${pilot} Ready`;
+    copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`;
+    if (fuelLevel < 10000 && cargoLevel >= 10000) {
+        list.style.faultyItems = "visible";
+        fuelStatus.innerHTML = `Fuel level too low for launch`;
+        cargoStatus.innerHTML = `Cargo mass too high for launch`;
+        launchStatus.innerHTML = `Shuttle not ready to launch`;
+        launchStatus.style.color = '#C7254E';
+    } else if (fuelLevel >= 10000 && cargoLevel >= 10000) {
+        list.style.faultyItems = "visible";
+        cargoStatus.innerHTML = `Cargo mass too high for launch`;
+        launchStatus.innerHTML = `Shuttle not ready to launch`;
+        launchStatus.style.color = '#C7254E';
+    } else if (fuelLevel < 10000 && cargoLevel < 10000) {
+        list.style.faultyItems = "visible";
+        fuelStatus.innerHTML = `Fuel level too low for launch`;
+        launchStatus.innerHTML = `Shuttle not ready to launch`;
+        launchStatus.style.color = '#C7254E';
+    } else {
+        launchStatus.innerHTML = `Shuttle is ready to launch`;
+        launchStatus.style.color = '#419F6A';
+    }
+   }
    
 }
 
 async function myFetch() {
     let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
+    planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then( function(response) {
+        return response.json();
+        });  
     return planetsReturned;
 }
 
 function pickPlanet(planets) {
+    let index = Math.floor(Math.random()*planets.length);
+    return planets[index];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
