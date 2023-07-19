44048df1278956e96c89aa5c48eed47b33522b69
Vi Fendrick <violetynaf@gmail.com>
2023-07-19 14:04:23 -0500

    done

--- scriptHelper.js
+++ scriptHelper.js
@@ -43,12 +43,12 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     list.style.visibility = "visible";
     pilotStatus.innerHTML = `Pilot ${pilot} Ready`;
     copilotStatus.innerHTML = `Co-pilot ${copilot} Ready`;
-    if (fuelLevel < 10000 && cargoLevel >= 10000) {
+    if (fuelLevel < 10000 && cargoLevel >= 10001) {
         fuelStatus.innerHTML = `Fuel level too low for launch`;
         cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
         launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
         launchStatus.style.color = '#C7254E';
-    } else if (fuelLevel >= 10000 && cargoLevel >= 10000) {
+    } else if (fuelLevel >= 10000 && cargoLevel >= 10001) {
         fuelStatus.innerHTML = `Fuel level high enough for launch`;
         cargoStatus.innerHTML = `Cargo mass too heavy for launch`;
         launchStatus.innerHTML = `Shuttle Not Ready for Launch`;
